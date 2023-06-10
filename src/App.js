c656092d31cba0704159a3bc6082c3662dd6557c
kishore-sundar7 <kishoresundar7@gmail.com>
2023-06-10 19:13:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [acceptCount, setAcceptCount] = useState(0)
+
+  const studentsData=[
+    {
+      name:"Kishore",
+      batch:"B47WD",
+      education:"FSD",
+    },
+    {
+      name:"Karthick",
+      batch:"F45",
+      education:"FE",
+    },
+    {
+      name:"Abi",
+      batch:"B45",
+      education:"B.E",
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Accepted Students :{acceptCount}</h1>
+      </div>
+
+      <div className='student-name'>
+        {studentsData.map((stud,idx)=>(
+          <StudentCard
+          key={idx}
+          name={stud.name}
+          batch={stud.batch}
+          education={stud.education}
+          acceptCount={acceptCount}
+          setAcceptCount={setAcceptCount}
+          />
+
+        )
+        )}
+      </div>
+      
+      
     </div>
   );
 }
 
 export default App;
+
+function StudentCard({name,batch,education,acceptCount,setAcceptCount}){
+
+  const[show, setShow] = useState(true)
+  
+  function handleAccept(){
+    setShow(!show)
+    setAcceptCount(acceptCount+1)
+  }
+  function handleReject(){
+    setShow(!show)
+    setAcceptCount(acceptCount -1)
+  }
+
+  return(
+  <div id='student' className='student-name'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_J4IeylQzW8W_6kSUF9c7seuRg757hfjyhejz6QFv&s'alt='name/'></img>
+    <h3> {name}</h3>
+    <p> {batch}</p>
+    <p>{education}</p>
+    {
+      show ? <button className='accept-btn btn'onClick={handleAccept}>Accept</button>
+    :
+    <button className='reject-btn btn'onClick={handleAccept}>Reject</button>
+}
+
+  </div>
+  )
+}
+// function Greeting2(){
+//   return(
+//     React.createElement('div',{id:"Welcome"},
+//     React.createElement('h1',{}, 'Hi'))
+//     )
+// }
